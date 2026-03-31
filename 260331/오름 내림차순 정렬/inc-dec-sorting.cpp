dcdae41d7889e44d1032da5639dc45dbeeaab671
Choi Seongwon <77625872+nowgnoesiohc@users.noreply.github.com>
2026-03-31 10:05:33 +0900

    Title: 오름 내림차순 정렬, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260331/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <functional>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+    }
+    
+    sort(v.begin(), v.end());
+
+    for(int i=0;i<n;i++){
+        cout << v[i] << " ";
+    }
+    cout << "\n";
+
+    sort(v.begin(), v.end(), greater<int>());
+
+    for(int i=0;i<n;i++){
+        cout << v[i] << " ";
+    }
+    return 0;
+}
