14adbdb25ecfba39f7688c571fac573aedb518b6
Choi Seongwon <77625872+nowgnoesiohc@users.noreply.github.com>
2026-05-29 19:49:01 +0900

    [Passed] 배열 채우고 출력하기 (1ms, 0MB)

---
+++ 260529/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a[11];
+
+    for( int i = 0; i < 10; i++){
+        cin >> a[i];
+    }
+
+    for ( int i = 9; i >=0; i --){
+        cout << a[i];
+    }
+
+    
+    return 0;
+}
