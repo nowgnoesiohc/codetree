087c5b029726fac713332c99002ae598842abecd
Choi Seongwon <77625872+nowgnoesiohc@users.noreply.github.com>
2026-06-05 19:50:14 +0900

    [Passed] 파스칼의 삼각형 (2ms, 0MB)

---
+++ 260605/파스칼의 삼각형/pascals-triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, arr[15][15] = {0};
+    cin >> N;
+
+    for( int i = 0; i < N; i++ ) {
+        for( int j = 0; j < i+1; j++ ) {
+            if( j == 0 || j == i ) arr[i][j] = 1;
+            else arr[i][j] = arr[i-1][j] + arr[i-1][j-1];
+        }
+    }
+
+    for( int i = 0; i < N; i++ ) {
+        for( int j = 0; j < i+1; j++ ) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
