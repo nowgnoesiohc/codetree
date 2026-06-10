452a7ce440081888c4cacda14e7a6fc4dbda296c
Choi Seongwon <77625872+nowgnoesiohc@users.noreply.github.com>
2026-06-10 16:05:41 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (2ms, 0MB)

---
+++ 260610/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void print () {
+    cout << "**********" << endl;
+}
+
+int main() {
+    print();
+    print();
+    print();
+    print();
+    print();
+    return 0;
+}
