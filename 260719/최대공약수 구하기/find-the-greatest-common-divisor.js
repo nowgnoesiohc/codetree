2b14b30b71c8dd4e8d48828caddd80cde0c7f4f9
Choi Seongwon <77625872+nowgnoesiohc@users.noreply.github.com>
2026-07-19 14:56:08 +0900

    [Passed] 최대공약수 구하기 (67ms, 10MB)

--- 260719/최대공약수 구하기/find-the-greatest-common-divisor.js
+++ 260719/최대공약수 구하기/find-the-greatest-common-divisor.js
@@ -3,28 +3,13 @@ const input = fs.readFileSync(0).toString().trim().split(" ");
 let n = Number(input[0]);
 let m = Number(input[1]);
 // Please Write your code here.
-
-const a = [...getMeasure(n)].reverse();
-const b = [...getMeasure(m)].reverse();
-
-loop1:for( const x of a ){
-    for( const y of b ){
-        if( x === y ) {
-            console.log(x)
-            break loop1;
-        }
+function gcd(a, b) {
+    while (b !== 0) {
+        const temp = a % b;
+        a = b;
+        b = temp;
     }
+    return a;
 }
 
-
-
-
-
-function getMeasure (x) {
-    const tmp = [];
-    for ( let i = 1; i <= x; i++ ){
-        if( x % i === 0 ) tmp.push(i); 
-    }
-    return tmp;
-}
-
+console.log(gcd(n,m))
